0ce40effba2dca3615865d3dd5f2eaee14b5d969
Briana-Koo <RoyalBlueSnow@gmail.com>
2021-11-18 10:16:53 -0600

    Initial Commit

---
+++ Daily Dose of Ducks/Daily_Dose_of_DucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_Dose_of_DucksApp.swift
+//  Daily Dose of Ducks
+//
+//  Created by Student on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Daily_Dose_of_DucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
